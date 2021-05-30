79b29f5b48ed2f2f698469ce9e651d2ebf9b83f8
Bubacuba <danielaskling03@gmail.com>
2021-05-30 21:56:21 +0200

    Riktiga dokumentet
    GitHub fackade med mig så ändrade till hur det ska se ut

--- WebbkodJavaScript.js
+++ WebbkodJavaScript.js
@@ -1,5 +1,14 @@
+function changeAbout(){
+    var idet = document.getElementsByTagName("div")[2];
+    document.getElementById("bodyabout").style.visibility = "visible"
+    document.getElementById("bodyculture").style.visibility = "hidden";
+    document.getElementById("bodytradition").style.visibility = "hidden";
+    document.getElementById("bodysights").style.visibility = "hidden";
+}
+
 function changeLink(){
     var idet = document.getElementsByTagName("div")[2];
+    document.getElementById("bodyabout").style.visibility = "hidden"
     document.getElementById("bodyculture").style.visibility = "visible";
     document.getElementById("bodytradition").style.visibility = "hidden";
     document.getElementById("bodysights").style.visibility = "hidden";
@@ -8,6 +17,7 @@ function changeLink(){
 
 function changeTradition(){
     var idet = document.getElementsByTagName("div")[2];
+    document.getElementById("bodyabout").style.visibility = "hidden"
     document.getElementById("bodyculture").style.visibility = "hidden";
     document.getElementById("bodytradition").style.visibility = "visible";
     document.getElementById("bodysights").style.visibility = "hidden";
@@ -15,6 +25,7 @@ function changeTradition(){
 
 function changeSights(){
     var idet = document.getElementsByTagName("div")[2];
+    document.getElementById("bodyabout").style.visibility = "hidden"
     document.getElementById("bodyculture").style.visibility = "hidden";
     document.getElementById("bodytradition").style.visibility = "hidden";
     document.getElementById("bodysights").style.visibility = "visible";
